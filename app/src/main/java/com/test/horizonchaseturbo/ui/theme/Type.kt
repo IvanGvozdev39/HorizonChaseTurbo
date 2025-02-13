7eb0150262f064811f9035964014c7099943ba92
Ivan Gvozdev <gvozde39@gmail.com>
2025-02-13 17:37:41 +0300

    Start Screen design layout

--- app/src/main/java/com/test/horizonchaseturbo/ui/theme/Type.kt
+++ app/src/main/java/com/test/horizonchaseturbo/ui/theme/Type.kt
@@ -2,15 +2,20 @@ package com.test.horizonchaseturbo.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.test.horizonchaseturbo.R
 
+val sfProFamily = FontFamily(
+    Font(R.font.sf_pro_text_heavy, FontWeight.Black)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = sfProFamily,
+        fontWeight = FontWeight.Black,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
