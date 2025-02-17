68020fe51cc7e7a77e25acd8e94c10f60a7464bf
Ivan Gvozdev <gvozde39@gmail.com>
2025-02-17 12:48:03 +0300

    Fix: dp values

--- app/src/main/java/com/test/horizonchaseturbo/presentation/screens/GameScreen.kt
+++ app/src/main/java/com/test/horizonchaseturbo/presentation/screens/GameScreen.kt
@@ -6,6 +6,14 @@ import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.runtime.*
+import androidx.compose.ui.geometry.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.platform.*
+import androidx.compose.ui.unit.*
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.*
+import androidx.compose.ui.res.*
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
@@ -16,6 +24,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
@@ -32,6 +41,7 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.test.horizonchaseturbo.R
@@ -58,8 +68,8 @@ fun GameScreen(navController: NavController) {
     ) {
         RoadMarkings(
             lineCount = 4,
-            markingHeight = 200f,
-            markingSpacing = 140f,
+            markingHeight = 101.dp,
+            markingSpacing = 51.dp,
             speed = 3f,
             modifier = Modifier.fillMaxSize()
         )
@@ -74,7 +84,7 @@ fun GameScreen(navController: NavController) {
                 painter = painterResource(id = R.drawable.player_car),
                 contentDescription = "Player Car",
                 modifier = Modifier
-                    .size(100.dp)
+                    .height(103.dp)
                     .graphicsLayer(
                         translationX = carOffsetX
                     )
@@ -113,14 +123,18 @@ fun GameScreen(navController: NavController) {
 @Composable
 fun RoadMarkings(
     lineCount: Int,
-    markingHeight: Float,
-    markingSpacing: Float,
+    markingHeight: Dp,
+    markingSpacing: Dp,
     speed: Float,
     modifier: Modifier = Modifier
 ) {
     val infiniteTransition = rememberInfiniteTransition(label = "")
+    val density = LocalDensity.current
+    val markingHeightPx = with(density) { markingHeight.toPx() }
+    val markingSpacingPx = with(density) { markingSpacing.toPx() }
+
     val offsetY by infiniteTransition.animateFloat(
-        initialValue = markingHeight + markingSpacing,
+        initialValue = markingHeightPx + markingSpacingPx,
         targetValue = 0f,
         animationSpec = infiniteRepeatable(
             animation = tween(durationMillis = (1000 / speed).toInt(), easing = LinearEasing),
@@ -133,22 +147,22 @@ fun RoadMarkings(
     Canvas(modifier = modifier.fillMaxSize()) {
         val canvasWidth = size.width
         val canvasHeight = size.height
-        val lineWidth = 40f
+        val lineWidth = with(density) { 15.dp.toPx() }
         val laneWidth = canvasWidth / lineCount
 
         for (i in 0 until lineCount) {
             val laneStartX = laneWidth * i + (laneWidth - lineWidth) / 2
 
-            val startYOffset = if (i % 2 == 0) 0f else markingHeight
+            val startYOffset = if (i % 2 == 0) 0f else markingHeightPx
 
             var currentY = -offsetY + startYOffset
             while (currentY < canvasHeight) {
                 drawRect(
                     color = markingColor,
                     topLeft = Offset(laneStartX, currentY),
-                    size = Size(lineWidth, markingHeight)
+                    size = Size(lineWidth, markingHeightPx)
                 )
-                currentY += markingHeight + markingSpacing
+                currentY += markingHeightPx + markingSpacingPx
             }
         }
     }
